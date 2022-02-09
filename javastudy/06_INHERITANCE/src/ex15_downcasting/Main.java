e7c4205ef4a003cb0b877b9ceac4c4f688477bdc
goodeeit <gt_min@naver.com>
2022-02-09 09:28:53 +0900

    2022-02-09

---
+++ javastudy/06_INHERITANCE/src/ex15_downcasting/Main.java
@@ -0,0 +1,19 @@
+package ex15_downcasting;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 10대의 자동차(Car, Ev, Hybrid)를 저장할 수 있는 배열 생성
+		
+		
+		// 10% 확률 Car, 30% 확률 Ev, 60% 확률 Hybrid
+		
+		// Car이면 drive() 호출
+		// Ev이면 charge() 호출
+		// Hybrid이면 addOil() 호출
+
+		
+	}
+
+}
