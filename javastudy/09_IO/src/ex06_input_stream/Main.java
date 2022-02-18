215cf2a9289d2f62f58473110c991214e385273f
goodeeit <gt_min@naver.com>
2022-02-18 13:07:51 +0900

    2022-02-18

--- javastudy/09_IO/src/ex06_input_stream/Main.java
+++ javastudy/09_IO/src/ex06_input_stream/Main.java
@@ -1,10 +1,18 @@
 package ex06_input_stream;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.util.List;
+
+import ex05_output_stream.User;
 
 public class Main {
 
@@ -83,8 +91,95 @@ public class Main {
 		
 	}
 	
+	
+	// 파일 복사하기
+	// 소요시간 출력
+	public static void quiz() {
+		
+		try {
+			
+			// 원본 읽기 스트림
+			BufferedInputStream bis = new BufferedInputStream(new FileInputStream("C:\\storage\\eclipse-jee-2021-03-R-win32-x86_64.zip"));
+			
+			// 복사본 생성하기 스트림
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("C:\\storage\\eclipse.zip"));
+			
+			byte[] b = new byte[1024];  // 1024바이트 = 1킬로바이트(KB)
+			int readCount;
+			
+			// 시작 시간
+			long start = System.currentTimeMillis();
+			
+			// 복사
+			while((readCount = bis.read(b)) != -1)
+				bos.write(b, 0, readCount);
+			
+			// 종료 시간
+			long end = System.currentTimeMillis();
+			
+			// 확인
+			System.out.println("복사 완료(" + (end - start) * 0.001 + "초 소요)");
+			
+			// 스트림 닫기
+			bos.close();
+			bis.close();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	// DataInputStream : 파일에 저장된 변수 읽는 스트림
+	public static void m3() {
+		
+		try {
+			
+			DataInputStream dis = new DataInputStream(new FileInputStream("C:\\storage\\b4.dat"));
+			
+			int age = dis.readInt();
+			double height = dis.readDouble();
+			String name = dis.readUTF();
+			
+			System.out.println(age + "," + height + "," + name);
+			
+			dis.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	// ObjectInputStream : 객체(인스턴스)를 그대로 입력받는 보조 스트림
+	public static void m4() {
+		
+		try {
+			
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:\\storage\\b5.dat"));
+			
+			List<User> users = (List<User>)ois.readObject();
+			for (User user : users) {
+				System.out.println(user);
+			}
+			
+			User user = (User)ois.readObject();
+			System.out.println(user);
+			
+			ois.close();
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		m2();
+		m4();
 	}
 
 }
