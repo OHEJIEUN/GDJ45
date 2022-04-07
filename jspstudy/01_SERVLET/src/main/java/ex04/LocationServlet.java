b460a84baed798f66662c3f972667e1b05d3713b
goodeeit <gt_min@naver.com>
2022-04-07 15:40:56 +0900

    2022-04-07

--- jspstudy/01_SERVLET/src/main/java/ex04/LocationServlet.java
+++ jspstudy/01_SERVLET/src/main/java/ex04/LocationServlet.java
@@ -2,6 +2,7 @@ package ex04;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,6 +27,25 @@ public class LocationServlet extends HttpServlet {
 		String name = request.getParameter("name");
 		String strAge = request.getParameter("age");
 		
+		// 디폴트 처리하기
+		// name은 "아무개", age는 0
+		
+		// Optional 클래스
+		// 1. null 처리를 위한 wrapper 클래스
+		// 2. jdk 1.8 이상 지원
+		// 3. 주요 메소드
+		//    1) 인수가 null이 아닌 경우만 처리
+		//        (1) 감싸기   : Optional.of(인수)
+		//        (2) 가져오기 : get()
+		//    2) 인수가 null인 경우도 처리 (우리가 사용할 내용)
+		//        (1) 감싸기   : Optional.ofNullable(인수)
+		//        (2) 가져오기 : orElse()
+		
+		Optional<String> optName = Optional.ofNullable(name);
+		String resName = optName.orElse("아무개");  // null: 아무개, not null: name
+		
+		Optional<String> optAge = Optional.ofNullable(strAge);
+		String resAge = optAge.orElse("0");
 		
 		
 		// 2. 응답
@@ -34,8 +54,8 @@ public class LocationServlet extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-		out.println("<h1>이름 " + name + "</h1>");
-		out.println("<h1>나이 " + strAge + "</h1>");
+		out.println("<h1>이름 " + resName + "</h1>");
+		out.println("<h1>나이 " + resAge + "</h1>");
 		out.println("<input type=\"button\" value=\"돌아가기\" onclick=\"history.back()\">");
 		
 		out.flush();
