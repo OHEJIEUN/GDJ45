7f7ec17ebafd5ccc1a7c3b9fdd2b71008d5f99a6
goodeeit <gt_min@naver.com>
2022-04-27 10:50:42 +0900

    2022-04-26

---
+++ jspstudy/08_JUNIT/src/main/java/test/ProductTestCase.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+// JUnit 단위 테스트
+
+// 1. DAO의 메소드 단위로 단위 테스트를 실시한다.
+// 2. Service 실행 결과가 "특정 값"인 경우 Service를 테스트 할 수도 있다.
+// 3. test 패키지 또는 src/main/test 소스 폴더에서 작업한다.
+// 4. 프로젝트의 Build Path에서 JUnit 라이브러리를 추가해야 한다.
+
+
+
+class ProductTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
