0e5c0bee8f93292f304da7f63de3040150d54d2f
goodeeit <gt_min@naver.com>
2022-05-10 10:15:52 +0900

    2022-05-10

--- springstudy/01_BEAN/src/main/java/com/goodee/ex01/java06/SpringBeanConfig.java
+++ springstudy/01_BEAN/src/main/java/com/goodee/ex01/java06/SpringBeanConfig.java
@@ -5,6 +5,10 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+// java/customer-context.xml에 만들어져 있는 bean을 가져오세요
+@ImportResource("java/customer-context.xml")
 
 @Configuration
 public class SpringBeanConfig {
