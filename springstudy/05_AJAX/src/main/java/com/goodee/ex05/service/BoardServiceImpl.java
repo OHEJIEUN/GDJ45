77f70bebbd7ad4fb6d84ac6fb5a35e22660abe06
goodeeit <gt_min@naver.com>
2022-05-13 17:00:58 +0900

    2022-05-13

--- springstudy/05_AJAX/src/main/java/com/goodee/ex05/service/BoardServiceImpl.java
+++ springstudy/05_AJAX/src/main/java/com/goodee/ex05/service/BoardServiceImpl.java
@@ -1,35 +1,40 @@
 package com.goodee.ex05.service;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 import com.goodee.ex05.domain.BoardDTO;
 
 public class BoardServiceImpl implements BoardService {
 
 	@Override
-	public BoardDTO detail1(HttpServlet request) {
-		// TODO Auto-generated method stub
-		return null;
+	public BoardDTO detail1(HttpServletRequest request) {
+		String title = request.getParameter("title");
+		Long hit = Long.parseLong(request.getParameter("hit"));
+		return new BoardDTO(title, hit);
 	}
 
 	@Override
 	public BoardDTO detail2(String title, Long hit) {
-		// TODO Auto-generated method stub
-		return null;
+		return new BoardDTO(title, hit);
 	}
 
 	@Override
 	public Map<String, Object> detail3(BoardDTO board) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("title", board.getTitle());
+		map.put("hit", board.getHit());
+		return map;
 	}
 
 	@Override
 	public BoardDTO detail4(Map<String, Object> map) {
-		// TODO Auto-generated method stub
-		return null;
+		BoardDTO board = new BoardDTO();
+		board.setTitle((String)map.get("title"));
+		board.setHit(Long.parseLong(map.get("hit").toString()));
+		return board;
 	}
 
 }
