65cc2276242f72fb63d80797f5296866b625b477
goodeeit <gt_min@naver.com>
2022-05-24 16:53:51 +0900

    2022-05-24

--- springstudy/11_PAGE_SEARCH/src/main/java/com/goodee/ex11/controller/EmployeeController.java
+++ springstudy/11_PAGE_SEARCH/src/main/java/com/goodee/ex11/controller/EmployeeController.java
@@ -1,11 +1,20 @@
 package com.goodee.ex11.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.goodee.ex11.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
 
+	@Autowired
+	private EmployeeService employeeService;
+	
 	@GetMapping("/")
 	public String index() {
 		return "index";
@@ -15,5 +24,18 @@ public class EmployeeController {
 	public String searchPage() {
 		return "employee/search";
 	}
+	
+	@GetMapping("/employee/list")
+	public String list(HttpServletRequest request, Model model) {
+		employeeService.findEmployees(request, model);
+		return "employee/search";  // search.jsp를 열면 list.jsp가 포함되어 있으므로 search.jsp로 간다.
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
