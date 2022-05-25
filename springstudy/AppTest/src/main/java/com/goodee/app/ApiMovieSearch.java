f37aa135d02f4a5c33aaa990f5bdc98d6256f444
goodeeit <gt_min@naver.com>
2022-05-25 19:45:28 +0900

    2022-05-25

--- springstudy/AppTest/src/main/java/com/goodee/app/ApiMovieSearch.java
+++ springstudy/AppTest/src/main/java/com/goodee/app/ApiMovieSearch.java
@@ -1,8 +1,11 @@
 package com.goodee.app;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -11,6 +14,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JOptionPane;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ApiMovieSearch {
 
