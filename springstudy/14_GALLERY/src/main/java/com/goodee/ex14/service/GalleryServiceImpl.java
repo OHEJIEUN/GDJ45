558cf4168797e0530c7ec6ff0960669151b9d424
goodeeit <gt_min@naver.com>
2022-06-02 11:47:54 +0900

    2022-06-02

--- springstudy/14_GALLERY/src/main/java/com/goodee/ex14/service/GalleryServiceImpl.java
+++ springstudy/14_GALLERY/src/main/java/com/goodee/ex14/service/GalleryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.goodee.ex14.service;
 
+import java.io.File;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,10 +8,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.goodee.ex14.domain.GalleryDTO;
 import com.goodee.ex14.mapper.GalleryMapper;
+import com.goodee.ex14.util.MyFileUtils;
 
 @Service
 public class GalleryServiceImpl implements GalleryService {
@@ -51,12 +54,54 @@ public class GalleryServiceImpl implements GalleryService {
 				.build();
 		
 		// Gallery Insert 수행
-		int galleryResult = galleryMapper.insertGallery(gallery);
-		
-		// FileAttachDTO에 galleryNo를 저장해서 GalleryMapper에 전달해야 한다.
-		// 방금 추가된 galleryNo를 누가 알고 있는가?
-		System.out.println(gallery);
+		System.out.println(gallery);  // INSERT 수행 전에는 gallery에 galleryNo값이 없다.
+		int galleryResult = galleryMapper.insertGallery(gallery);  // INSERT 수행
+		System.out.println(gallery);  // INSERT 수행 후에는 selectKey에 의해서 gallery에 galleryNo값이 저장된다.
 
+		// 결론. FILE_ATTACH 테이블에 INSERT할 galleryNo 정보는 gallery에 저장되어 있다.
+		
+		// 파일 첨부
+		List<MultipartFile> files = multipartRequest.getFiles("files");  // 파라미터 files
+		
+		for (MultipartFile file : files) {
+			
+			// 예외 처리는 기본으로 필요함.
+			try {
+				
+				// 첨부가 없을 수 있으므로 점검해야 함.
+				if(file != null && file.isEmpty() == false) {  // 첨부가 있다.(둘 다 필요함)
+					
+					// 첨부파일의 본래 이름(origin)
+					String origin = file.getOriginalFilename();
+					origin = origin.substring(origin.lastIndexOf("\\") + 1);  // IE는 본래 이름에 전체 경로가 붙어서 파일명만 빼야 함.
+					
+					// 첨부파일의 저장된 이름(saved)
+					String saved = MyFileUtils.getUuidName(origin);
+					
+					// 첨부파일의 저장 경로(디렉터리)
+					String path = MyFileUtils.getTodayPath();
+					
+					// 저장 경로(디렉터리) 없으면 만들기
+					File dir = new File(path);
+					if(dir.exists() == false) {
+						dir.mkdirs();
+					}
+					
+					// 첨부파일
+					File uploadFile = new File(dir, saved);
+					
+					System.out.println(origin);
+					System.out.println(saved);
+					System.out.println(path);
+					
+				}
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 
 	@Override
